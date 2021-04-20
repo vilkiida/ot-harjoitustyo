c78ad8e545d78e55bbde646d2d84bc5d982f9a44
Iida Vilkkinen <iida.vilkkinen@gmail.com>
2021-04-20 20:21:01 +0300

    tests created

--- src/tests/minesweeper_test.py
+++ src/tests/minesweeper_test.py
@@ -1,20 +1,43 @@
 import unittest
 from minesweeper import Main
 
-LEVEL_MAP= [[0,0,0,0,1,0,0,0],
-            [0,1,0,0,0,0,1,0],
-            [0,0,0,0,0,0,0,0],
-            [0,1,0,0,1,0,0,0],
-            [0,1,0,0,0,1,0,0],
-            [0,1,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,1],
-            [0,1,0,0,0,0,0,0]]
-
-CELL_SIZE = 50
+easy=(9,9,10)
+mediumhard=(16,16,40)
+expert=(16,30,99)
 
 class TestMinesweeper(unittest.TestCase):
-    def setUp(self):
-        pass
+    def test_game_height_right_when_easy(self):
+        self.minesweeper = Main(easy)
+        self.assertEqual(9, self.minesweeper.height)
+    
+    def test_game_width_right_when_easy(self):
+        self.minesweeper = Main(easy)
+        self.assertEqual(9, self.minesweeper.width)
+    
+    def test_game_mine_amount_right_when_easy(self):
+        self.minesweeper = Main(easy)
+        self.assertEqual(10,self.minesweeper.mine_amount)
+    
+    def test_game_height_right_when_mediumhard(self):
+        self.minesweeper = Main(mediumhard)
+        self.assertEqual(16, self.minesweeper.height)
 
-
-
+    def test_game_width_right_when_easy(self):
+        self.minesweeper = Main(mediumhard)
+        self.assertEqual(16, self.minesweeper.width)
+    
+    def test_game_mine_amount_right_when_mediumhard(self):
+        self.minesweeper = Main(mediumhard)
+        self.assertEqual(40, self.minesweeper.mine_amount)
+    
+    def test_game_height_right_when_expert(self):
+        self.minesweeper = Main(expert)
+        self.assertEqual(16, self.minesweeper.height)
+    
+    def test_game_width_right_when_expert(self):
+        self.minesweeper = Main(expert)
+        self.assertEqual(30, self.minesweeper.width)
+    
+    def test_game_mine_amount_right_when_expert(self):
+        self.minesweeper = Main(expert)
+        self.assertEqual(99, self.minesweeper.mine_amount)
