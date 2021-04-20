a7dcd8afe9eeb2833cb17b61a82fae8019511f90
Iida Vilkkinen <iida.vilkkinen@gmail.com>
2021-04-20 20:21:33 +0300

    code modified

--- src/cell.py
+++ src/cell.py
@@ -12,7 +12,6 @@ class Cell:
 
         if self.opened == False:
             self.image = pygame.image.load(os.path.join(dirname, "assets", "ms-unopened.png"))
-            self.rect=self.image.get_rect()
     
     def is_a_mine(self):
         return self.mine
@@ -56,52 +55,38 @@ class Cell:
             if self.mine == False:
                 if self.neighbour_mines == 0:
                     self.image = pygame.image.load(os.path.join(dirname, "assets", "ms-opened.png"))
-                    self.rect=self.image.get_rect()
                 elif self.neighbour_mines == 1:
                     self.image = pygame.image.load(os.path.join(dirname, "assets", "ms-1.png"))
-                    self.rect=self.image.get_rect()
                 elif self.neighbour_mines == 2:
                     self.image = pygame.image.load(os.path.join(dirname, "assets", "ms-2.png"))
-                    self.rect=self.image.get_rect()
                 elif self.neighbour_mines == 3:
                     self.image = pygame.image.load(os.path.join(dirname, "assets", "ms-3.png"))
-                    self.rect=self.image.get_rect()
                 elif self.neighbour_mines == 4:
                     self.image = pygame.image.load(os.path.join(dirname, "assets", "ms-4.png"))
-                    self.rect=self.image.get_rect()
                 elif self.neighbour_mines == 5:
                     self.image = pygame.image.load(os.path.join(dirname, "assets", "ms-5.png"))
-                    self.rect=self.image.get_rect()
                 elif self.neighbour_mines == 6:
                     self.image = pygame.image.load(os.path.join(dirname, "assets", "ms-6.png"))
-                    self.rect=self.image.get_rect()
                 elif self.neighbour_mines == 7:
                     self.image = pygame.image.load(os.path.join(dirname, "assets", "ms-7.png"))
-                    self.rect=self.image.get_rect()
                 elif self.neighbour_mines == 8:
                     self.image = pygame.image.load(os.path.join(dirname, "assets", "ms-8.png"))
-                    self.rect=self.image.get_rect()
-
             else:
                 self.image = pygame.image.load(os.path.join(dirname, "assets", "ms-mine.png"))
-                self.rect=self.image.get_rect()
     
     def mark_flagg(self):
         self.flagged = True
         self.image = pygame.image.load(os.path.join(dirname, "assets", "ms-flag.png"))
-        self.rect = self.image.get_rect()
     
     def mark_questionmark(self):
         self.flagged = False
         self.questionmark = True
         self.image = pygame.image.load(os.path.join(dirname, "assets", "ms-qm.png"))
-        self.rect = self.image.get_rect()
 
     def remove_markings(self):
         self.flagged = False
         self.questionmark = False
         self.image = pygame.image.load(os.path.join(dirname, "assets", "ms-unopened.png")) 
-        self.rect = self.image.get_rect()
     
     def blow_up(self):
         self.image = pygame.image.load(os.path.join(dirname, "assets", "ms-mine2.png"))
--- src/minesweeper.py
+++ src/minesweeper.py
@@ -2,5 +2,21 @@ import pygame
 from game import Game
 from field import Field
 
-board = Field(10,8,13)
-Game(board,50)
+easy=(9,9,10)
+mediumhard=(16,16,40)
+expert=(16,30,99)
+
+class Main:
+    def __init__(self, difficulty):
+        self.height=difficulty[0]
+        self.width=difficulty[1]
+        self.mine_amount=difficulty[2]
+
+        self.board=Field(self.height,self.width,self.mine_amount)
+    def run_game(self):
+        Game(self.board, 50)
+
+
+if __name__ == "__main__":
+    minesweeper=Main(easy)
+    minesweeper.run_game()
