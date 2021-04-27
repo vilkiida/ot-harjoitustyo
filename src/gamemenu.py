8e032fde5a3c4ea55786f44d29648700fdbc8fa6
Iida Vilkkinen <iida.vilkkinen@gmail.com>
2021-04-27 15:04:05 +0300

    problem fixed where could open cells that were flagged and questionmarked and main menu and gamemanu texts added

--- src/gamemenu.py
+++ src/gamemenu.py
@@ -8,18 +8,16 @@ class GameMenu:
         self.screen_width = 500
         self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
         self.easy_button = pygame.Rect(50, 75, 400, 100)
-        self.easy_text = "EASY"
         self.easy = (9, 9, 10)
         self.mediumhard_button = pygame.Rect(50, 250, 400, 100)
-        self.mediumhard_text = "MEDIUMHARD"
         self.mediumhard = (16, 16, 40)
         self.expert_button = pygame.Rect(50, 425, 400, 100)
-        self.expert_text = "EXPERT"
         self.expert = (16, 30, 99)
-        self.button_color = (180, 180, 180)
+        self.button_color = (140, 140, 150)
         self.background_color = (70, 70, 70)
         self.cell_size=50
         pygame.init()
+        self.font = pygame.font.SysFont("Arial", 50)
         self.loop()
     def click(self, position):
         if self.easy_button.collidepoint(position):
@@ -33,6 +31,9 @@ class GameMenu:
             Game(expert_field, self.cell_size)
     def draw_button(self, button):
         pygame.draw.rect(self.screen, self.button_color, button)
+    def draw_text(self, text, x, y):
+        button_text = self.font.render(text, True, (0, 0, 0))
+        self.screen.blit(button_text, (x, y))
     def check_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -44,8 +45,11 @@ class GameMenu:
     def draw_screen(self):
         self.screen.fill(self.background_color)
         self.draw_button(self.easy_button)
+        self.draw_text("EASY", self.easy_button.left+140, self.easy_button.top+25)
         self.draw_button(self.mediumhard_button)
+        self.draw_text("MEDIUMHARD", self.mediumhard_button.left+28, self.mediumhard_button.top+25)
         self.draw_button(self.expert_button)
+        self.draw_text("EXPERT", self.expert_button.left+108, self.expert_button.top+25)
         pygame.display.flip()
     def loop(self):
         while True:
--- src/minesweeper.py
+++ src/minesweeper.py
@@ -6,23 +6,18 @@ class MainMenu:
         self.screen_width = 500
         self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
         self.gamemenu_button = pygame.Rect(50, 75, 400, 100)
-        self.gamemenu_text = "PLAY"
         self.highscores_button = pygame.Rect(50, 250, 400, 100)
-        self.highscores_text = "HIGHSCORES"
         self.instructions_button = pygame.Rect(50, 425, 400, 100)
-        self.instructions_text = "HELP"
-        self.button_color = (180, 180, 180)
+        self.button_color = (140, 140, 150)
         self.background_color = (50, 50, 50)
-        #self.font = pygame.font.SysFont(None, 50)
         pygame.init()
+        self.font = pygame.font.SysFont("Arial", 50)
         self.loop()
-
     def draw_button(self, button):
         pygame.draw.rect(self.screen, self.button_color, button)
-        # button_text = self.font.render(text, True, (0, 0, 0))
-        # x=button.left + 100
-        # y=button.top + 25
-        # self.screen.blit(button_text, x, y)
+    def draw_text(self, text, x, y):
+        button_text = self.font.render(text, True, (0, 0, 0))
+        self.screen.blit(button_text, (x, y))
     def click(self, position):
         if self.gamemenu_button.collidepoint(position):
             GameMenu()
@@ -30,7 +25,6 @@ class MainMenu:
             print("highscores")
         if self.instructions_button.collidepoint(position):
             print("help")
-
     def check_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -39,18 +33,17 @@ class MainMenu:
                 if event.button == 1:
                     position = pygame.mouse.get_pos()
                     self.click(position)
-
     def draw_screen(self):
         self.screen.fill(self.background_color)
         self.draw_button(self.gamemenu_button)
+        self.draw_text("PLAY", self.gamemenu_button.left+140, self.gamemenu_button.top+25)
         self.draw_button(self.highscores_button)
+        self.draw_text("HIGHSCORES", self.highscores_button.left+33, self.highscores_button.top+25)
         self.draw_button(self.instructions_button)
-
+        self.draw_text("HELP", self.instructions_button.left+140, self.instructions_button.top+25)
         pygame.display.flip()
-
     def loop(self):
         while True:
             self.check_events()
             self.draw_screen()
-
 MainMenu()
